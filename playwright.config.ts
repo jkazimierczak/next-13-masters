c3b5d055df7bcc979a12d4b555ce6d707e403e00
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-08 13:20:51 +0200

    fix(tooling): Added missing playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+require("dotenv").config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+	testDir: "./.",
+	fullyParallel: false,
+	workers: 1,
+	reporter: [["line"], ["json", { outputFile: "test-results.json" }]],
+	timeout: 10000,
+	use: {
+		baseURL: process.env.NEXT13MASTERS_URL,
+		trace: "on-first-retry",
+	},
+
+	projects: [
+		{
+			name: "Google Chrome",
+			use: { ...devices["Desktop Chrome"], channel: "chrome" },
+		},
+	],
+});
