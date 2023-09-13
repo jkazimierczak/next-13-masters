e622bb42de32534b1dd434faca02045e1730b458
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-13 16:28:01 +0200

    refactor(app): Extract common layout type

--- src/app/product/[productId]/layout.tsx
+++ src/app/product/[productId]/layout.tsx
@@ -1,3 +1,5 @@
-export default function ProductPageLayout({ children }: { children: React.ReactNode }) {
+import { type Layout } from "@/types/layout";
+
+export default function ProductPageLayout({ children }: Layout) {
 	return <article className="mx-auto max-w-screen-2xl p-7">{children}</article>;
 }
--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,9 +1,10 @@
 import { type Metadata } from "next";
+import { type Layout } from "@/types/layout";
 
 export const metadata: Metadata = {
 	title: "Products",
 };
 
-export default function ProductsLayout({ children }: { children: React.ReactNode }) {
+export default function ProductsLayout({ children }: Layout) {
 	return <main className="mx-auto max-w-screen-2xl">{children}</main>;
 }
