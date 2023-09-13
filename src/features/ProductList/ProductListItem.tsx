5912dc1431bd3abda46c84ac9a184269b9262c8e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-13 18:07:25 +0200

    fix(components): Wrapped ProductListItem with a link

--- src/features/ProductList/ProductListItem.tsx
+++ src/features/ProductList/ProductListItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { type Product } from "./types";
 import { ProductImage } from "./ProductImage";
 import { formatPrice } from "@/utils/formatPrice";
@@ -7,20 +8,22 @@ type ProductListItemProps = {
 };
 
 export function ProductListItem({
-	product: { title, category, price, image },
+	product: { id, title, category, price, image },
 }: ProductListItemProps) {
 	return (
 		<li className="inline-block w-fit sm:w-72">
-			<article>
-				<div className="overflow-hidden rounded border border-neutral-300">
-					<ProductImage src={image} alt={title} height={500} width={500} />
-				</div>
-				<div className="my-1 flex justify-between">
-					<h3 className="text-xl font-bold">{title}</h3>
-				</div>
-				<p>{formatPrice(price)}</p>
-				<p hidden={true}>{category}</p>
-			</article>
+			<Link href={`/product/${id}`}>
+				<article>
+					<div className="overflow-hidden rounded border border-neutral-300">
+						<ProductImage src={image} alt={title} height={500} width={500} />
+					</div>
+					<div className="my-1 flex justify-between">
+						<h3 className="text-xl font-bold">{title}</h3>
+					</div>
+					<p>{formatPrice(price)}</p>
+					<p hidden={true}>{category}</p>
+				</article>
+			</Link>
 		</li>
 	);
 }
