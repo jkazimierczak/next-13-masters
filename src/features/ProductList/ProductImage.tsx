6a90651d342ae2f9995ee8000ffabdb38f61149e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-13 18:07:01 +0200

    fix(components): Add missing type

--- src/features/ProductList/ProductImage.tsx
+++ src/features/ProductList/ProductImage.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
-import { type ProductImage } from "./types";
 
-type ProductImageProps = ProductImage;
+type ProductImageProps = {
+	width: number;
+	height: number;
+	src: string;
+	alt: string;
+};
 
 export function ProductImage({ src, alt, width, height, ...props }: ProductImageProps) {
 	return (
