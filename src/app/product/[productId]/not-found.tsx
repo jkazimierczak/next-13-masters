33d43c3c5589d7b4bc5afe6319b294424db1ec66
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-15 14:09:06 +0200

    refactor(app): Enable title in not-found.tsx

--- src/app/product/[productId]/not-found.tsx
+++ src/app/product/[productId]/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-	// title: "Product not found",  // TODO: https://github.com/vercel/next.js/issues/45620
+	title: "Product not found",
 	robots: {
 		index: false,
 		follow: true,
