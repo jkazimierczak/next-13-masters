894b0e5de8a07d8d58feec74e0ae343249cd5999
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-15 16:25:08 +0200

    feat(app): Add simple pagination

--- src/app/products/[page]/page.tsx
+++ src/app/products/[page]/page.tsx
@@ -1,6 +1,11 @@
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import { clsx } from "clsx";
 import { ProductList } from "@/features/ProductList/ProductList";
 import { getProducts } from "@/api/products";
+
+const TOTAL_PRODUCT_COUNT = 4205;
+const PAGES = Math.ceil(TOTAL_PRODUCT_COUNT / 20);
 
 type ProductsPageProps = {
 	params: {
@@ -17,9 +22,26 @@ export default async function ProductsPage({ params: { page } }: ProductsPagePro
 
 	const products = await getProducts(pageNum);
 
+	const isPrevPageLinkDisabled = pageNum === 1;
+	const prevPageLink = isPrevPageLinkDisabled ? `/products/${pageNum}` : `/products/${pageNum - 1}`;
+
+	const isNextPageLinkDisabled = pageNum === PAGES;
+	const nextPageLink = isNextPageLinkDisabled ? `/products/${pageNum}` : `/products/${pageNum + 1}`;
+
 	return (
-		<main className="p-8">
+		<main className="mx-auto max-w-screen-2xl p-8">
 			<div className="mx-auto w-fit">
+				<nav className="flex gap-4" aria-label="pagination">
+					<Link href={prevPageLink} className={clsx(isPrevPageLinkDisabled && "text-neutral-300")}>
+						Prev
+					</Link>
+					<p>
+						{page} of {PAGES}
+					</p>
+					<Link href={nextPageLink} className={clsx(isNextPageLinkDisabled && "text-neutral-300")}>
+						Prev
+					</Link>
+				</nav>
 				<ProductList products={products} data-testid="products-list" />
 			</div>
 		</main>
