7b159d975eb04d759712e536f839196771df3a97
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-18 13:46:50 +0200

    feat(components): Add rating

--- src/features/ProductList/ProductListItem.tsx
+++ src/features/ProductList/ProductListItem.tsx
@@ -8,19 +8,22 @@ type ProductListItemProps = {
 };
 
 export function ProductListItem({
-	product: { id, title, category, price, image },
+	product: { id, title, category, price, image, rating },
 }: ProductListItemProps) {
 	return (
 		<li className="inline-block w-fit sm:w-72">
 			<Link href={`/product/${id}`}>
 				<article>
-					<div className="overflow-hidden rounded border border-neutral-300">
+					<div className="overflow-hidden rounded border border-neutral-300/50">
 						<ProductImage src={image} alt={title} height={500} width={500} />
 					</div>
 					<div className="my-1 flex justify-between">
 						<h3 className="text-xl font-bold">{title}</h3>
 					</div>
-					<p>{formatPrice(price)}</p>
+					<div className="flex justify-between">
+						<span>{formatPrice(price)}</span>
+						<span className="text-neutral-500">{rating.rate.toFixed(1)} ★</span>
+					</div>
 					<p hidden={true}>{category}</p>
 				</article>
 			</Link>
