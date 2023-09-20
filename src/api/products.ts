b29028a8e629c1dba6d615a172d7e45fb86b15db
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-20 17:17:47 +0200

    feat(app): Implement category views

--- src/api/products.ts
+++ src/api/products.ts
@@ -2,6 +2,8 @@ import { type Product } from "@/features/ProductList/types";
 import { executeGraphql } from "@/api/graphql";
 import {
 	ProductGetByIdDocument,
+	ProductsGetByCategorySlugDocument,
+	ProductsGetCountByCategorySlugDocument,
 	ProductsGetCountDocument,
 	ProductsGetListDocument,
 } from "@/gql/graphql";
@@ -13,6 +15,32 @@ export async function getProducts(page: number): Promise<Product[]> {
 	});
 
 	return gqlRes.products.map((p) => {
+		return {
+			id: p.id,
+			title: p.name,
+			price: p.price,
+			description: p.description,
+			category: p.categories[0]?.name || "",
+			image: p.images[0] && {
+				src: p.images[0].url,
+				alt: p.name,
+			},
+		};
+	});
+}
+
+export async function getProductsByCategorySlug(slug: string, page: number) {
+	const gqlRes = await executeGraphql(ProductsGetByCategorySlugDocument, {
+		slug,
+		count: 20,
+		offset: 20 * (page - 1),
+	});
+
+	if (!gqlRes.categories[0]) {
+		return null;
+	}
+
+	return gqlRes.categories[0].products.map((p) => {
 		return {
 			id: p.id,
 			title: p.name,
@@ -50,6 +78,12 @@ export async function getProductById(productId: string) {
 
 export async function getProductsCount() {
 	const gqlRes = await executeGraphql(ProductsGetCountDocument, {});
+
+	return gqlRes.productsConnection.aggregate.count;
+}
+
+export async function getProductsCountByCategorySlug(slug: string) {
+	const gqlRes = await executeGraphql(ProductsGetCountByCategorySlugDocument, { slug });
 
 	return gqlRes.productsConnection.aggregate.count;
 }
---
+++ src/app/categories/[categoryName]/[page]/page.tsx
@@ -0,0 +1,53 @@
+import { notFound } from "next/navigation";
+import { getProductsByCategorySlug, getProductsCountByCategorySlug } from "@/api/products";
+import { Pagination } from "@/features/Pagination";
+import { ProductList } from "@/features/ProductList/ProductList";
+
+type CategoryPageParams = {
+	params: {
+		categoryName: string;
+		page: string;
+	};
+};
+
+export default async function CategoryPage({ params: { page, categoryName } }: CategoryPageParams) {
+	const pageNum = Number(page);
+
+	if (pageNum <= 0 || isNaN(pageNum)) {
+		notFound();
+	}
+
+	const products = await getProductsByCategorySlug(categoryName, pageNum);
+	if (!products) {
+		notFound();
+	}
+
+	const totalProductCount = await getProductsCountByCategorySlug(categoryName);
+
+	return (
+		<main className="mx-auto max-w-screen-2xl p-8">
+			<div className="mx-auto w-fit">
+				<header className="mb-4 flex items-center justify-between">
+					<h1 className="border-b border-secondary text-3xl font-bold">{categoryName}</h1>
+					<div className="hidden sm:block">
+						<Pagination
+							currentPage={pageNum}
+							itemsPerPage={20}
+							totalItems={totalProductCount}
+							link={"/products"}
+						/>
+					</div>
+				</header>
+				<ProductList products={products} data-testid="products-list" />
+				<div className="mt-4 flex justify-center">
+					<Pagination
+						currentPage={pageNum}
+						itemsPerPage={20}
+						totalItems={totalProductCount}
+						link={"/products"}
+					/>
+				</div>
+			</div>
+		</main>
+	);
+}
