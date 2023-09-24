c398f4ef1626523f88b26358292ea9a6bda0cd5e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-09-24 16:45:04 +0200

    refactor(app): Improved styling of product images

--- src/features/ProductList/ProductListItem.tsx
+++ src/features/ProductList/ProductListItem.tsx
@@ -7,6 +7,18 @@ type ProductListItemProps = {
 	product: Product;
 };
 
+function prepareImageProps(images: ProductListItemProps["product"]["images"]) {
+	return images && images[0]
+		? {
+				src: images[0].src,
+				alt: images[0].alt,
+		  }
+		: {
+				src: "/placeholder-img.webp",
+				alt: "Placeholder image of album cover.",
+		  };
+}
+
 export function ProductListItem({
 	product: { id, title, category, price, images },
 }: ProductListItemProps) {
@@ -14,12 +26,8 @@ export function ProductListItem({
 		<li className="inline-block w-fit sm:w-72">
 			<Link href={`/product/${id}`}>
 				<article>
-					<div className="overflow-hidden rounded border border-neutral-300/50">
-						{images && images[0] ? (
-							<ProductImage src={images[0].src} alt={images[0].alt} height={500} width={500} />
-						) : (
-							<div></div>
-						)}
+					<div className="overflow-hidden rounded transition-shadow hover:shadow">
+						<ProductImage {...prepareImageProps(images)} height={500} width={500} />
 					</div>
 					<div className="my-1 flex justify-between">
 						<h3 className="text-xl font-bold">{title}</h3>
