043c349f8d832736ac1538300a6f8ecea638afbb
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-10-05 11:39:28 +0200

    feat(app): Add simple cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 import { getCartFromCookies } from "@/api/cart";
+import { formatPrice } from "@/lib/utils";
 
 export default async function CartPage() {
 	const cart = await getCartFromCookies();
@@ -9,8 +10,34 @@ export default async function CartPage() {
 	}
 
 	return (
-		<>
+		<div>
+			<h1>Order #{cart.id} summary</h1>
+
 			<pre>{JSON.stringify(cart, null, 2)}</pre>
-		</>
+
+			<table>
+				<thead>
+					<tr>
+						<th>Product</th>
+						<th>Quantity</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{cart.orderItems.map((item) => {
+						if (!item.product) {
+							return null;
+						}
+						return (
+							<tr key={item.product.id}>
+								<td>{item.product.name}</td>
+								<td>{item.quantity}</td>
+								<td>{formatPrice(item.product.price / 100)}</td>
+							</tr>
+						);
+					})}
+				</tbody>
+			</table>
+		</div>
 	);
 }
