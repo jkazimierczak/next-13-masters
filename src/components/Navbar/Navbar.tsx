cd7d4344779faba1c8bf940b6bb3d818698ce34c
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-10-07 21:30:05 +0200

    feat(app): Add cart to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 import { type Route } from "next";
+import { ShoppingCart } from "lucide-react";
 import { Search } from "./Search";
 import { ActiveLink } from "@/features/ActiveLink/ActiveLink";
+import { getCartFromCookies } from "@/api/cart";
 
 const links: { href: Route; name: string; exact: boolean }[] = [
 	{ href: "/", name: "Home", exact: true },
@@ -11,7 +13,11 @@ const links: { href: Route; name: string; exact: boolean }[] = [
 	{ href: "/categories/reggae-and-dub" as Route, name: "Reggae & Dub", exact: false },
 ];
 
-export function Navbar() {
+export async function Navbar() {
+	const cart = await getCartFromCookies();
+	// TODO: Count total quantity
+	const quantity = cart?.orderItems.length ?? 0;
+
 	return (
 		<nav className="sticky flex w-full items-center justify-between bg-background px-5 py-4 text-white shadow md:px-12 lg:px-24">
 			<div className="flex items-center gap-8">
@@ -28,7 +34,17 @@ export function Navbar() {
 					))}
 				</ul>
 			</div>
+
 			<Search />
+
+			<Link href={"/cart"}>
+				<div className="flex items-center gap-2">
+					<span className="h-6 w-6 rounded-full bg-primary text-center text-sm font-semibold">
+						{quantity}
+					</span>
+					<ShoppingCart />
+				</div>
+			</Link>
 		</nav>
 	);
 }
