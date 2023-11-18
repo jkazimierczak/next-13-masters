f755067a40ecaf8b6ba72a02cbdf6df8a49934b9
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-19 00:47:41 +0100

    fix: Fix issues related to dependencies update

--- src/components/Cart/CartItemQuantity.tsx
+++ src/components/Cart/CartItemQuantity.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { experimental_useOptimistic as useOptimistic } from "react";
+import React, { useOptimistic } from "react";
 import { Button } from "@/components/ui/button";
 import { setProductQuantityAction } from "@/app/cart/actions";
 
