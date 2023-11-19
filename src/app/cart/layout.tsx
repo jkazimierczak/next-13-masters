3dd1281a05d3f6416f5d326deb73f0b69bffe9fb
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-20 00:21:07 +0100

    fix: Fix header styles

--- src/app/cart/layout.tsx
+++ src/app/cart/layout.tsx
@@ -1,10 +1,5 @@
 import { type Layout } from "@/types/layout";
 
 export default async function CartLayout({ children }: Layout) {
-	return (
-		<div className="mx-auto max-w-screen-sm px-4 py-6 xl:max-w-screen-lg">
-			<h1 className="mb-4 w-fit border-b border-secondary text-3xl font-bold">Your Cart</h1>
-			{children}
-		</div>
-	);
+	return <div className="mx-auto max-w-screen-sm px-4 py-6 xl:max-w-screen-lg">{children}</div>;
 }
