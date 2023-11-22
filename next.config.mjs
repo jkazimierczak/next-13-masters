b5d09ae404ccc73be81ca93fe204efa0b0efe8c0
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-23 00:51:05 +0100

    refactor: Remove compiler config

--- next.config.mjs
+++ next.config.mjs
@@ -5,38 +5,30 @@ const isProduction = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		domains: ["media.graphassets.com"],
-	},
-	pageExtensions: ["ts", "tsx", "mdx"],
-	experimental: {
-		typedRoutes: true,
-		mdxRs: true,
-		serverActions: true,
-	},
-	compiler: {
-		// removeConsole: isProduction
-		// 	? {
-		// 			exclude: ["error"],
-		// 	  }
-		// 	: false,
-	},
-	rewrites: () => {
-		return [
-			{
-				source: "/products",
-				destination: "/products/1",
-			},
-			{
-				source: "/categories/:slug",
-				destination: "/categories/:slug/1",
-			},
-			{
-				source: "/collections/:slug",
-				destination: "/collections/:slug/1",
-			},
-		];
-	},
+    images: {
+        domains: ["media.graphassets.com"],
+    },
+    pageExtensions: ["ts", "tsx", "mdx"],
+    experimental: {
+        typedRoutes: true,
+        mdxRs: true,
+    },
+    rewrites: () => {
+        return [
+            {
+                source: "/products",
+                destination: "/products/1",
+            },
+            {
+                source: "/categories/:slug",
+                destination: "/categories/:slug/1",
+            },
+            {
+                source: "/collections/:slug",
+                destination: "/collections/:slug/1",
+            },
+        ];
+    },
 };
 
 const configWithMDX = withMDX();
